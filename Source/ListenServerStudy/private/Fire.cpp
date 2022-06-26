cf9c98df7817b7b09f0bc7017caba1d661803f8f
Oriburger <uss425@naver.com>
2022-06-26 21:59:57 +0900

    화재 진압 기능 추가

--- Source/ListenServerStudy/private/Fire.cpp
+++ Source/ListenServerStudy/private/Fire.cpp
@@ -19,12 +19,9 @@ AFire::AFire()
 	FireEmitter = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("FIRE_EMITTER"));
 	FireEmitter->SetupAttachment(DefaultSceneRoot);
 
-	SmokeEmitter = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SMOKE_EMITTER"));
-	SmokeEmitter->SetupAttachment(DefaultSceneRoot);
-	SmokeEmitter->SetWorldScale3D({ 0.0f, 0.0f, 0.0f });
-	if(SmokeMaterialInterface != nullptr) //BP에서 Mat Interface를 지정해주었다면
-		SmokeDynamicMaterial = UMaterialInstanceDynamic::Create(SmokeMaterialInterface, this);
-	SmokeEmitter->SetMaterial(0, SmokeDynamicMaterial);
+	SteamEmitter = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Steam_EMITTER"));
+	SteamEmitter->SetupAttachment(DefaultSceneRoot);
+	SteamEmitter->SetWorldScale3D({ 0.0f, 0.0f, 0.0f });
 }
 
 // Called when the game starts or when spawned
@@ -32,6 +29,8 @@ void AFire::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//Steam Emitter의 Dynamic Material Instance 초기화
+	InitSteamDynamicMaterial();
 }
 
 // Called every frame
@@ -42,68 +41,71 @@ void AFire::Tick(float DeltaTime)
 	if (HasAuthority()) //서버에서 실행되는 경우?
 	{
 		MulticastUpdateEmitterScale(FireEmitter, { FireScaleSize, FireScaleSize, FireScaleSize }); //Multicast 호출
-		MulticastUpdateEmitterScale(SmokeEmitter, { SmokeScaleSize, SmokeScaleSize, SmokeScaleSize });
+		MulticastUpdateEmitterScale(SteamEmitter, { SteamScaleSize, SteamScaleSize, SteamScaleSize });
 	}
 	else //클라이언트에서 실행되는 경우?
 	{
 		ServerRPCUpdateEmitterScale(FireEmitter, { FireScaleSize, FireScaleSize, FireScaleSize }); //Server RPC 호출
-		ServerRPCUpdateEmitterScale(SmokeEmitter, { SmokeScaleSize, SmokeScaleSize, SmokeScaleSize });
-	}	
+		ServerRPCUpdateEmitterScale(SteamEmitter, { SteamScaleSize, SteamScaleSize, SteamScaleSize });
+	}
+
+	if (IsReadyToDestroy) //Destroy될 준비가 되었다면? (불이 모두 꺼졌다면)
+	{
+		TryDestroy(); //Destroy를 시도
+	}
 }
 
-void AFire::MulticastUpdateEmitterScale_Implementation(UParticleSystemComponent* target, const FVector& newScale)
+void AFire::MulticastUpdateEmitterScale_Implementation(UParticleSystemComponent* Target, const FVector& NewScale)
 {
-	if (newScale.Y < 0.0f || newScale.Y > 3.0f) return;
-	target->SetWorldScale3D(FMath::VInterpTo(target->GetComponentScale(), newScale, 0.1f, 0.25f)); //매 Tick 마다 FireScaleSize를 업데이트한다.
+	if (NewScale.Y < 0.0f || NewScale.Y > 3.0f) return;
+	Target->SetWorldScale3D(FMath::VInterpTo(Target->GetComponentScale(), NewScale, 0.1f, 0.25f)); //매 Tick 마다 FireScaleSize를 업데이트한다.
 }
 
-void AFire::ServerRPCUpdateEmitterScale_Implementation(UParticleSystemComponent* target, const FVector& newScale)
+void AFire::ServerRPCUpdateEmitterScale_Implementation(UParticleSystemComponent* Target, const FVector& NewScale)
 {
-	MulticastUpdateEmitterScale(target, newScale);
+	MulticastUpdateEmitterScale(Target, NewScale); //서버와 연결된 클라이언트들에게 모두 실행
+}
+
+void AFire::InitSteamDynamicMaterial()
+{
+	if (SteamMaterialInterface == nullptr) return; //BP에서 Mat Interface를 지정해주지 않았다면 반환
+	SteamDynamicMaterial = UMaterialInstanceDynamic::Create(SteamMaterialInterface, GetWorld());
+	SteamEmitter->SetMaterial(0, SteamDynamicMaterial);	
+}
+
+void AFire::UpdateSteamDynamicMaterial()
+{
+	if (SteamOpacityValue < 0.0f) return; //SteamOpacity가 유효하지 않은 값이라면?
+	SteamOpacityValue -= 0.1f; //새로운 Opacity값으로 대체
+	SteamDynamicMaterial->SetScalarParameterValue("Opacity", FMath::FInterpTo(SteamOpacityValue+0.1f, SteamOpacityValue, 0.05f, 0.5f));  
 }
 
 float AFire::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
 {
 	if (DamageCauser == nullptr && !DamageCauser->ActorHasTag("FireHose")) return 0; //DamageCause이 유효하지 않거나 FireHose가 아니라면 return 
-	if (FireScaleSize == 0) TryDestroy(); //불이 모두 꺼졌다면 Destroy
+	if (FireScaleSize == 0) SetSteamDisappearTimer(); //불이 모두 꺼졌다면 Destroy
 
 	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser); //Super 클래스의 TakeDamage 호출
 
 	FireScaleSize = FMath::Max(0.0f, FireScaleSize - DamageAmount * FMath::FRandRange(1.25f, 1.75f));  //FireScale을 DamageAmount만큼 뺌, (자연스러운 전환위해 rand값 추가)
-	SmokeScaleSize = FMath::Min(3.0f, SmokeScaleSize + DamageAmount * FMath::FRandRange(3.0f, 5.0f));  //SmokeScale값도 위와 마찬가지로 업데이트
+	SteamScaleSize = FMath::Min(3.0f, SteamScaleSize + DamageAmount * FMath::FRandRange(3.0f, 5.0f));  //SteamScale값도 위와 마찬가지로 업데이트
 
 	return DamageAmount;
 }
 
-void AFire::TryDestroy()
+void AFire::SetSteamDisappearTimer()
 {
-	FTimerHandle DestroyTimerHandle;
-	const float SmokeLifeSpan = 15.0f; //연기의 Life (특정 시간만큼 Opacity 업데이트)
-	GetWorldTimerManager().SetTimer(DestroyTimerHandle, 1.0f, true, 200.0f); //타이머 설정
+	if (IsReadyToDestroy) return; //이미 Destroy가 진행중이라면
+	IsReadyToDestroy = true; //Destroy될 준비가 되었다고 체크
 
-	//if(SmokeDynamicMaterial == nullptr) 
-	//	GetWorldTimerManager().ClearTimer(DestroyTimerHandle);
+	FTimerHandle DestroyTimerHandle; //DestroyTimer 핸들
+	float SteamLifeSpan = 10.0f; //연기의 Life (특정 시간만큼 Opacity 업데이트)
+	GetWorldTimerManager().SetTimer(DestroyTimerHandle, this, &AFire::UpdateSteamDynamicMaterial, 0.5f, true, 0.0f); //타이머 설정
+}
 
-	float OpacityValue = 1.0f; //초기 Opacity
-
-	if (GEngine)
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow,
-			FString::Printf(TEXT("%.5lf"), GetWorldTimerManager().GetTimerRemaining(DestroyTimerHandle)));
-
-	while (GetWorldTimerManager().IsTimerActive(DestroyTimerHandle)) //Timer이 끝날때까지 반복
-	{
-		if (GEngine)
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, 
-					 FString::Printf(TEXT("%.5lf"), GetWorldTimerManager().GetTimerRemaining(DestroyTimerHandle)));
-
-		//Timer이 끝날때까지 반복
-		SmokeDynamicMaterial->SetScalarParameterValue("Opacity", OpacityValue);
-		OpacityValue -= 0.1f;
-	}
-
-	if (GEngine)
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow,
-			FString::Printf(TEXT("%.5lf"), GetWorldTimerManager().GetTimerRemaining(DestroyTimerHandle)));
-
+inline void AFire::TryDestroy()
+{
+	if (SteamOpacityValue > 0) return; //수증기까지 다 사라지면 Destroy
 	Destroy();
+	return;
 }
--- Source/ListenServerStudy/public/Fire.h
+++ Source/ListenServerStudy/public/Fire.h
@@ -11,9 +11,8 @@
 /*
  - Name        : AFire
  - Descirption : FireHose의 물에 닿으면 꺼지는 불 액터
- - Date        : 2022/06/22 LJH
+ - Date        : 2022/06/26 LJH
 */
-
 
 UCLASS(Category = "FireFighter")
 class LISTENSERVERSTUDY_API AFire : public AActor
@@ -24,6 +23,41 @@ public:
 	// Sets default values for this actor's properties
 	AFire();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//불과 수증기 Emitter의 Scale을 업데이트 (멀티캐스트)
+	UFUNCTION(NetMulticast, reliable)//NetMulticast, Reliable)
+		void MulticastUpdateEmitterScale(UParticleSystemComponent* Target, const FVector& NewScale);
+
+	//불과 수증기 Emitter의 Scale을 업데이트 (서버에서 실행)
+	UFUNCTION(Server, reliable)
+		void ServerRPCUpdateEmitterScale(UParticleSystemComponent* Target, const FVector& NewScale);
+
+	//Steam(불 진압 시 나오는 수증기)의 Opacity 값 조절을 위한 동적 머티리얼을 생성
+	UFUNCTION()
+		void InitSteamDynamicMaterial();
+
+	//불이 꺼지고 수증기가 소멸하는 효과를 위해 Opacity값을 Update한다
+	UFUNCTION()
+		void UpdateSteamDynamicMaterial();
+
+	//FireHose의 NS_Emiiter가 Apply하는 데미지를 받는다. FireScaleSize를 DamageAmount 만큼 줄임
+	UFUNCTION()
+		virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
+
+	//수증기가 사라지는 Timer를 설정하여 소멸을 시뮬레이션
+	UFUNCTION()
+		void SetSteamDisappearTimer();
+
+	//Destroy할 준비가 되었다면, Destroy를 시도 (수증기가 모두 사라지면 Destroy)
+	UFUNCTION()
+		void TryDestroy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 private:
 	UPROPERTY(VisibleDefaultsOnly)
 		USceneComponent* DefaultSceneRoot;
@@ -32,40 +66,25 @@ private:
 		UParticleSystemComponent* FireEmitter; //불 이미터
 
 	UPROPERTY(EditAnywhere, Category = "VFX")
-		UParticleSystemComponent* SmokeEmitter; //연기 효과 이미터 (화재 진압 로직 시)
+		UParticleSystemComponent* SteamEmitter; //연기 효과 이미터 (화재 진압 로직 시)
 
 	UPROPERTY(EditAnywhere, Category = "VFX")
-		UMaterialInterface* SmokeMaterialInterface; //연기 이미터 원본 머티리얼 
+		UMaterialInterface* SteamMaterialInterface; //연기 이미터 원본 머티리얼 
 
 	UPROPERTY()
-		UMaterialInstanceDynamic* SmokeDynamicMaterial; //연기 이미터  Opacity 조절을 위한 다이나믹 머티리얼
+		UMaterialInstanceDynamic* SteamDynamicMaterial; //연기 이미터  Opacity 조절을 위한 다이나믹 머티리얼
 
 	UPROPERTY(VisibleDefaultsOnly, Category = "Gameplay")
-		float FireScaleSize = 1.0f;   //불의 크기 (범위 : 0<=size<=1)
+		float FireScaleSize = 1.0f;   //불 이미터의 크기 (범위 : 0<=size<=1)
 
 	UPROPERTY(VisibleDefaultsOnly, Category = "Gameplay")
-		float SmokeScaleSize = 0.0f;
+		float SteamScaleSize = 0.0f;  //수증기 이미터의 크기 
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY(VisibleDefaultsOnly, Category = "Gameplay")
+		float SteamOpacityValue = 1.0f; //수증기 이미터의 Opacity
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY()
+		bool IsReadyToDestroy = false; 
 
-	//Emitter의 Scale을 업데이트
-	UFUNCTION(NetMulticast, reliable)//NetMulticast, Reliable)
-		void MulticastUpdateEmitterScale(UParticleSystemComponent* target, const FVector& newScale);
 
-	//Emitter의 Scale을 업데이트 
-	UFUNCTION(Server, reliable)
-		void ServerRPCUpdateEmitterScale(UParticleSystemComponent* target, const FVector& newScale);
-
-	//FireHose의 NS_Emiiter가 Apply하는 데미지를 받는다. FireScaleSize를 DamageAmount 만큼 줄임.
-	UFUNCTION()
-		virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
-
-	UFUNCTION()
-		void TryDestroy();
 };
